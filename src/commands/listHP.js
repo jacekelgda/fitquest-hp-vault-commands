a0299f20b0838b84f5e735285988d866a048f3a7
Jacek Ławniczak <jacekelgda@gmail.com>
2017-11-14 13:29:24 +0100

    Filters --closes #9

--- src/commands/listHP.js
+++ src/commands/listHP.js
@@ -13,15 +13,63 @@ class ListHPCommand {
       try {
         let message = '';
         if (isAdmin(msg.user_name)) {
-          const data = await this.hpService.getAll();
-          for (let i = 0; i < data.length; i += 1) {
-            message += `${data[i].userName} - ${data[i].hp} \n`;
+          let data = await this.hpService.getAll();
+          data = data.sort((a, b) => {
+            return parseInt(b.hp) - parseInt(a.hp);
+          });
+          const hasFilter = /^filter:.*/.test(msg.text);
+          let filter = '';
+          if (hasFilter) {
+            filter = msg.text.substring(('filter:').length, msg.text.length);
+            switch (true) {
+              case (['alive','live','living','not dead','active','playing'].indexOf(filter) > -1): {
+                  for (let i = 0; i < data.length; i += 1) {
+                    if (data[i].hp > 0) {
+                      message += `:heart: ${data[i].userName} ${data[i].hp}\n`;
+                    }
+                  }
+                  break;
+              }
+              case (['dead','killed','not alive','not playing','done'].indexOf(filter) > -1): {
+                  for (let i = 0; i < data.length; i += 1) {
+                    if (data[i].hp < 1) {
+                      message += `:candle: ${data[i].userName}\n`;
+                    }
+                  }
+                  break;
+              }
+              case /[0-5]+/.test(filter): {
+                  for (let i = 0; i < data.length; i += 1) {
+                    if (data[i].hp == parseInt(filter)) {
+                      if (data[i].hp < 1) {
+                        message += `:candle: ${data[i].userName} ${data[i].hp}\n`;
+                      } else if (data[i].hp > 0) {
+                        message += `:heart: ${data[i].userName} ${data[i].hp}\n`;
+                      }
+                    }
+                  }
+                  break;
+              }
+              default: {
+                  message = 'Please try again.';
+              }
+            }
+            bot.replyPrivate((message == '') ? 'Noting found by that filter' : message);
+          } else {
+            for (let i = 0; i < data.length; i += 1) {
+              if (data[i].hp < 1) {
+                message += `:candle: ${data[i].userName} ${data[i].hp}\n`;
+              } else if (data[i].hp > 0) {
+                message += `:heart: ${data[i].userName} ${data[i].hp}\n`;
+              }
+            }
+            bot.replyPrivate(message);
           }
-          bot.replyPrivate(message);
         } else {
           bot.replyPrivate('Access denied');
         }
       } catch (error) {
+        console.log(error);
         bot.replyPrivate('Whoops! An Error occured!');
       }
     });
